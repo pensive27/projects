85e2d94ff57b42552aab3dfb10a72b077c2db8d7
pensive27 <67540777+pensive27@users.noreply.github.com>
2020-08-21 15:44:36 +0100

    Add files via upload
    A turn tracker for tabletop games such as Dungeons and Dragons. Made for my convenience. My very first project!

---
+++ initiative.py
@@ -0,0 +1,73 @@
+print("Copyright 2020. Please feel free to use the code. However, please give credit to user under the following Discord application username: SirNeil email: sk992@live.com\n"
+	  "Make sure to enter all fields as the programme will prompt renetry for ALL characters otherwise.")
+character_listname = []
+character_listinitiative = []
+aclist = []
+hplist = []
+round = 0
+
+
+while True:
+	try:
+		characters = int(input("How many character(s) are there?: "))
+		if characters <= 0:
+			print("There must be characters to create a scene!")
+			continue
+	except ValueError:
+		print("Please enter a value preferably more than 0")
+		continue
+	else:
+		break
+
+for num in range(characters):
+	while True:
+		try:
+			nam_char = (input("Enter character name: "))
+			initiative = int(input("Character's initiative: "))
+			ac = int(input("Enter AC: "))
+			hp = int(input("Enter HP: "))
+		except ValueError:
+			print("Please enter integers only and don't leave blanks! Programme will now restart.")
+			continue
+		except NameError:
+			print("Please enter names using letters only! Please enter all character details")
+			continue
+		else:
+			character_listname.append(nam_char)
+			character_listinitiative.append(initiative)
+			aclist.append(ac)
+			hplist.append(hp)
+			break
+
+character_listinitiative, character_listname, aclist, hplist = (list(t) for t in zip(*reversed(sorted(zip(character_listinitiative, character_listname, aclist, hplist)))))
+# Merges the lists and sorts them in reverse order. The function ensures they don't become tuples.
+
+for num in range(0, characters):
+	while round >= 0:
+		round += 1
+		print("\nRound ", round)
+		print(f"Next is: {character_listname[num]}\nThe AC: {aclist[num]}\nThe HP: {hplist[num]}\ninitiative: {character_listinitiative[num]}\n")
+		for postion, number in enumerate(hplist):
+			try:
+				print(f"Enter health change for {character_listname[num]}:\n current Hp: {number}\n")
+				modifier = int(input("Enter health change  (use + 'number',- 'number' or 0 if no change): "))
+				result = number + modifier
+				hplist[postion] = result
+				print("To exit programme simply close.")
+			except ValueError:
+				print("Please enter only integers and don't leave blanks!")
+				continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
